9036d8ff059d629ae5bf61d8e5b40b2c0d4c3063
Ag Stephens <ag.stephens@stfc.ac.uk>
2017-07-12 23:01:08 +0100

    Added new obs vocabularies and script to convert.

---
+++ src/climate_grid_vocabs/README.md
@@ -0,0 +1,16 @@
+These vocabs are taken from the climate-grid packages. 
+
+The source is a single Excel file and we need to create JSON files from it.
+
+The code to do that translation is here...
+
+Notes from provider:
+
+```
+The worksheets of most interest are:
+
+CF_metadata
+grid_properties
+projected_crs
+geographic_crs
+```
---
+++ src/climate_grid_vocabs/write_obs_cvs.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+"""
+write_obs_cvs.py
+================
+
+Writes JSON CV files from the `config_tables.xls` vocabs from Climate-Grid package.
+
+"""
+
+# Third-party imports
+import pandas
+import simplejson
+import numpy
+
+CONFIG_XLS = 'config_tables.xls'
+json_tmpl = {
+    "version_metadata":{
+        "author":"Ag Stephens <ag.stephens@stfc.ac.uk>",
+        "creation_date":"Tue Jun 06 07:45:06 2017 -0100",
+        "institution_id":"STFC",
+        "previous_commit":""
+    }
+}
+
+sheet_map = {'CF_metadata': ('variables', 'short_name'),
+             'grid_properties': ('grid_properties', 'grid_name'),
+             'projected_crs': ('projected_crs', 'epsg'), 
+             'geographic_crs': ('geographic_crs', 'epsg')}
+
+
+def _serialise(name, content):
+    output_path = "../../UKCP18_obs_{}.json".format(name)
+    resp = json_tmpl.copy()
+    resp[name] = content
+
+    with open(output_path, 'w') as writer:
+        simplejson.dump(resp, writer, indent=4, sort_keys=True)
+
+    print "Wrote: {}".format(output_path)
+
+
+def write_json(sheet, name, key_name):
+    df = pandas.read_excel(CONFIG_XLS, sheetname=sheet, na_values="")
+    t = df.T # transposed
+    variables = {}
+
+    for i in range(t.shape[1]):
+        data = dict(t[i])
+        d = {}
+    
+        for key, value in data.items():
+            if type(value) == type(numpy.nan): value = ""
+            d[key] = value
+
+        sn = d[key_name]
+
+        if sn:
+            variables[sn] = d.copy()
+
+    _serialise(name, variables)
+
+
+def main():
+    for sheet, (name, key_name) in sheet_map.items():
+        write_json(sheet, name, key_name)
+
+if __name__ == "__main__":
+
+    main()
+
