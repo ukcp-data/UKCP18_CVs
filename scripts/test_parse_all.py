4b3207a0f49f609cba92f03a14efa9d4b5eddadd
Ag Stephens <ag.stephens@stfc.ac.uk>
2019-09-02 15:04:52 +0000

    Updated variables

--- scripts/test_parse_all.py
+++ scripts/test_parse_all.py
@@ -5,9 +5,10 @@ def test_parsing_all_jsons():
 
     jsons = glob.glob("*.json")
     if len(jsons) < 5:
-        jsons = glob.glob("../*.json")
+        jsons = glob.glob("../UKCP18_CVs/*.json")
 
     for _json in jsons:
+        print('Testing: {0}'.format(_json))
         with open(_json) as reader:
             try:
                 data = json.load(reader)
